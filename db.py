9beadc10e21e9f80f3485d4ec53c373ab45dc53d
isaactony <isaactonyloi@gmail.com>
2024-05-19 15:06:31 +0300

    Learning how to create rest apis with flask

---
+++ db.py
@@ -0,0 +1,14 @@
+stores = {}
+
+items = {
+    1: {"name": "Item 1", "price": 10.99},
+    2: {"name": "Item 2", "price": 15.99},
+    3: {"name": "Item 3", "price": 7.49},
+    4: {"name": "Item 4", "price": 22.00},
+    5: {"name": "Item 5", "price": 5.99},
+    6: {"name": "Item 6", "price": 3.50},
+    7: {"name": "Item 7", "price": 12.30},
+    8: {"name": "Item 8", "price": 9.99},
+    9: {"name": "Item 9", "price": 14.75},
+    10: {"name": "Item 10", "price": 20.00}
+}
