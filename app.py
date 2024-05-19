06e7b892cdf15cdcee293adce4fd851c91d418e5
isaactony <isaactonyloi@gmail.com>
2024-05-19 11:34:26 +0300

    Learning how to create rest apis with flask

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
 
 @app.get("/store")
 def get_stores():
@@ -10,19 +20,35 @@ def get_stores():
 
 
 @app.post("/store")
-def post_stores():
+def create_store():
     request_data = request.get_json()
     new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
 
 
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
 
-@app.get("/store/<string:name>/item")
-def get_specific_item():
+
+@app.get("/store/<string:name>")
+def get_store(name):
     for store in stores:
         if store["name"] == name:
             return store
-    return "Store not found"
+    return {"message": "Store not found"}, 404
 
 
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
