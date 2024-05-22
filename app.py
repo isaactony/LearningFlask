f051eb96dbf0846f0a6fb12290a3a25d9df7c0b9
isaactony <isaactonyloi@gmail.com>
2024-05-22 09:31:15 +0300

    Learning how to create rest apis with flask

--- app.py
+++ app.py
@@ -63,12 +63,20 @@ def get_store(store_id):
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
 
     return store
-
 
 @app.get("/store")
 def get_stores():
