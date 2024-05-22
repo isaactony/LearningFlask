1098e5e92af9ed5bb37fbcb35c17c045c3801371
isaactony <isaactonyloi@gmail.com>
2024-05-22 09:24:38 +0300

    Learning how to create rest apis with flask

--- app.py
+++ app.py
@@ -14,22 +14,37 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")ß
 
 
 
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+    # Here not only we need to validate data exists,
+    # But also what type of data. Price should be a float,
+    # for example.
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
 
     return item
-
 
 @app.get("/item")
 def get_all_items():
@@ -44,6 +59,7 @@ def get_store(store_id):
         return stores[store_id]
     except KeyError:
         abort(404, message="Store not found.")
+
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
